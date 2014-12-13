9b4326d13986b38b41ed74436e9d19d64f022acc
Ikuo Degawa <degawa@ikuwow.com>
2014-12-13 15:11:00 +0900

    Initial Commit

---
+++ LavacakeTests/LavacakeTests.swift
@@ -0,0 +1,36 @@
+//
+//  LavacakeTests.swift
+//  LavacakeTests
+//
+//  Created by 出川幾夫 on 12/13/14.
+//  Copyright (c) 2014 Lavacake. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class LavacakeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
