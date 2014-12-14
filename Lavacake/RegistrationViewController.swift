7cff77a363e46fc46370701608c8bd6ce2f15c94
Ikuo Degawa <degawa@ikuwow.com>
2014-12-14 15:45:42 +0900

    preapareforsegueを設定

--- Lavacake/RegistrationViewController.swift
+++ Lavacake/RegistrationViewController.swift
@@ -24,15 +24,8 @@ class RegistrationViewController: UIViewController {
     @IBAction func unwindToTop(segue: UIStoryboardSegue) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        println("tset")
     }
-    */
 
 }
